fce3bdd5ecea8a6cedd899b397401ee967a4c71b
sergio <serbatero>
2019-05-20 01:27:42 -0500

    Peticiones Listas GraphiQL

--- tumusica_api/src/lists/resolvers.js
+++ tumusica_api/src/lists/resolvers.js
@@ -3,7 +3,7 @@ import { url, port } from './server';
 
 const URL = `http://${url}:${port}`;
 const URLListVinculations = `http://${url}:${port}/list_vinculations`;
-
+const URLList = `http://${url}:${port}/lists`;
 
 const resolvers = {
 	Query: {
@@ -11,6 +11,11 @@ const resolvers = {
 			getRequest(URLListVinculations, ''),
 		ListVinculationsById: (_, { id }) =>
 			generalRequest(`${URLListVinculations}/${id}`, 'GET'),
+
+		allList: (_) =>
+			getRequest(URLList, ''),
+		ListById: (_, { id }) =>
+			generalRequest(`${URLList}/${id}`, 'GET'),
 	},
 	Mutation: {
 		createListVinculations: (_, { listvinculations }) =>
@@ -18,7 +23,14 @@ const resolvers = {
 		updateListVinculations: (_, { id, listvinculations }) =>
 			generalRequest(`${URLListVinculations}/${id}`, 'PUT', listvinculations),
 		deleteListVinculations: (_, { id }) =>
-			generalRequest(`${URLListVinculations}/${id}`, 'DELETE')
+			generalRequest(`${URLListVinculations}/${id}`, 'DELETE'),
+
+		createList: (_, { list }) =>
+			generalRequest(`${URLList}`, 'POST', list),
+		updateList: (_, { id, list }) =>
+			generalRequest(`${URLList}/${id}`, 'PUT', list),
+		deleteList: (_, { id }) =>
+			generalRequest(`${URLList}/${id}`, 'DELETE')
 	}
 };
 
--- tumusica_api/src/lists/typeDefs.js
+++ tumusica_api/src/lists/typeDefs.js
@@ -1,13 +1,28 @@
 export const listsTypeDef = `
+type List {
+    id: Int
+    name: String!
+    image: String!
+    user_id: Int!
+    list_vinculations: [ListVinculations]
+ }
+
+ input ListInput {
+    name: String!
+    image: String!
+    user_id: Int!
+ }
+
 type ListVinculations {
     id: Int
     user_id: Int!
-    list_id: Int!
+    list: List
 }
 input ListVinculationsInput {
     user_id: Int!
     list_id: Int!
-}`;
+}
+`;
 
 
 
@@ -15,10 +30,16 @@ input ListVinculationsInput {
 export const listsQueries = `
     allListVinculations: [ListVinculations]!
     ListVinculationsById(id: Int!): ListVinculations!
+    allList: [List]!
+    ListById(id: Int!): List!
 `;
 
 export const listsMutations = `
     createListVinculations(listvinculations: ListVinculationsInput!): ListVinculations!
     deleteListVinculations(id: Int!): Int
     updateListVinculations(id: Int!, listvinculations: ListVinculationsInput!): ListVinculations!
+
+    createList(list: ListInput!): List!
+    deleteList(id: Int!): Int
+    updateList(id: Int!, list: ListInput!): List!
 `;
