200b0ea60aadbf26c8f9f61c10b248797955bf58
Nicolás Campuzano <nicolas.campuzano@tulpep.com>
2019-07-16 00:32:33 -0500

    get tokens error

--- tumusica_api/src/lists/resolvers.js
+++ tumusica_api/src/lists/resolvers.js
@@ -37,61 +37,67 @@ const resolvers = {
 		createList: async(_, { list }) => {
       generalRequest(`${URLList}`, 'POST', list);
       const tokens = await usersResolvers.Query.getTokens(list.user_id);
-      // SEND TO MOBILE
-      let requestBody = [];
-      let tokenPhone = [];
-      tokens.map(element => {
-        if (element.tokenType == 1) {
-          if (!tokenPhone.includes(element.token)) {
-            tokenPhone.push(element.token);
-            requestBody.push({
+      console.log(tokens);
+      try {
+        // SEND TO MOBILE
+        let requestBody = [];
+        let tokenPhone = [];
+        tokens.map(element => {
+          if (element.tokenType == 1) {
+            if (!tokenPhone.includes(element.token)) {
+              tokenPhone.push(element.token);
+              requestBody.push({
+                data: {
+                  message: "Has creado una nueva lista",
+                  type: "LIST_CREATED",
+                  body: "Felicidades puedes adicionar a tu lista tu musica preferida"
+                },
+                title: "Has creado una nueva lista",
+                body: "Felicidades puedes adicionar a tu lista tu musica preferida",
+                to: element.token
+              });
+            }
+          }
+          return true;
+        });
+        request({
+          uri: "https://exp.host/--/api/v2/push/send", 
+          headers: { 'Content-Type': 'application/json' },
+          method: "POST",
+          body: requestBody,
+          json: true
+        });
+
+        // SEND TO WEB
+        requestBody = {};
+        tokens.map(element => {
+          if (element.tokenType == 2) {
+            requestBody = {
               data: {
                 message: "Has creado una nueva lista",
                 type: "LIST_CREATED",
                 body: "Felicidades puedes adicionar a tu lista tu musica preferida"
               },
-              title: "Has creado una nueva lista",
-              body: "Felicidades puedes adicionar a tu lista tu musica preferida",
               to: element.token
-            });
+            };
           }
-        }
-        return true;
-      });
-      request({
-        uri: "https://exp.host/--/api/v2/push/send", 
-        headers: { 'Content-Type': 'application/json' },
-        method: "POST",
-        body: requestBody,
-        json: true
-      });
-
-      // SEND TO WEB
-      requestBody = {};
-      tokens.map(element => {
-        if (element.tokenType == 2) {
-          requestBody = {
-            data: {
-              message: "Has creado una nueva lista",
-              type: "LIST_CREATED",
-              body: "Felicidades puedes adicionar a tu lista tu musica preferida"
-            },
-            to: element.token
-          };
-        }
-        return true;
-      });
-      request({
-        uri: "https://fcm.googleapis.com/fcm/send",
-        method: "POST",
-        headers: { 
-          'Content-Type': 'application/json',
-          'Authorization': 'key=AAAAfBIen2Q:APA91bHiZJJ-8juaH41ZrTC72c0t7YVXexG5aV0TlLIkY4x5UlFxZcEWWsTO7zEAkkYtRs_MF9jpL5E-3h7E-jArTCMNnFe1kz1cxjldPn_Fyfx2Z84tStSSSDTGNlzGn29R2LQVnDmA',
-        },
-        body: requestBody,
-        json: true
-      });
-      return list;  
+          return true;
+        });
+        request({
+          uri: "https://fcm.googleapis.com/fcm/send",
+          method: "POST",
+          headers: { 
+            'Content-Type': 'application/json',
+            'Authorization': 'key=AAAAfBIen2Q:APA91bHiZJJ-8juaH41ZrTC72c0t7YVXexG5aV0TlLIkY4x5UlFxZcEWWsTO7zEAkkYtRs_MF9jpL5E-3h7E-jArTCMNnFe1kz1cxjldPn_Fyfx2Z84tStSSSDTGNlzGn29R2LQVnDmA',
+          },
+          body: requestBody,
+          json: true
+        });
+        return list;
+      } catch(e) {
+        return list;
+      }
+        
     },
 		updateList: (_, { id, list }) =>
 			generalRequest(`${URLList}/${id}`, 'PUT', list),
